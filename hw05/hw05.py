5e117d931df2dd0b08fc7a7201682bf32f4a239a
siky <siqi.lu1@ucdconnect.ie>
2020-10-18 12:22:15 +0100

    Q2,Q3 not finalised

--- hw05/hw05.py
+++ hw05/hw05.py
@@ -151,6 +151,11 @@ def store_digits(n):
     >>> print("Do not use str or reversed!") if any([r in cleaned for r in ["str", "reversed"]]) else None
     """
     "*** YOUR CODE HERE ***"
+    if n < 10:
+        return Link(n)
+    else:
+        link = Link(n%10)
+        return Link(store_digits(n//10), link)
 
 
 def is_bst(t):
